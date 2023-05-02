055d908eb34dc3d9ee4c9d010c15396561e7f4c0
Susie Gordon <susieyoungchoi@gmail.com>
2023-05-02 13:35:42 -0400

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -19,6 +19,18 @@ for (i=0; i<201; i++) {
 // Fizz Buzz
 ////////////////////////////////
 
+for (i=1; i<101; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log('FizzBuzz');
+    }else if (i % 3 === 0) {
+        console.log('Fizz');
+    }else if (i % 5 === 0)  {
+        console.log('Buzz'); 
+    }else {
+        console.log(i);
+    }
+        
+    }
 
 ////////////////////////////////
 // Wild Wild Life
